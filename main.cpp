91f0ea9a914a4e60cee7d41d1c17e65f3c3aecc3
Qing Zheng <chingzheng3905@gmail.com>
2019-11-04 00:06:34 -0500

    .

--- main.cpp
+++ main.cpp
@@ -3,15 +3,25 @@
 #include <cctype>
 #include "funcs.h"
 #include <fstream>
-int
-main ()
+
+int main ()
 {
   std::string line;
   std::string output;
-  while (getline (std::cin, line))
-    {
-      output += format (line);
-    }
-  std::cout << output << std::endl;
-}
 
+  // edited
+  std::ifstream in("bad-code.cpp");
+  std::streambuf *cinbuf = std::cin.rdbuf();
+  std::cin.rdbuf(in.rdbuf());
+
+  std::ofstream out("out.txt");
+  std::streambuf *coutbuf = std::cout.rdbuf();
+  std::cout.rdbuf(out.rdbuf());
+
+  while (getline (std::cin, line))
+      output += format (line);
+  std::cout << output;
+
+  std::cin.rdbuf(cinbuf);
+  std::cout.rdbuf(coutbuf);
+}
